4025682c516f22e98bcee8edf324f8da1de1906b
robbyxsuganda <robbyxsuganda@gmail.com>
2025-01-29 18:01:49 +0700

    feat[cms]:edit games

--- client/CMS/src/views/AddGame.jsx
+++ client/CMS/src/views/AddGame.jsx
@@ -48,7 +48,17 @@ export default function AddGame() {
 
       navigate("/");
     } catch (error) {
-      console.log("Error:", error);
+      Toastify({
+        text: `${error.response.data.message}`,
+        duration: 3000,
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function () {}, // Callback after click
+      }).showToast();
     }
   };
 
--- client/CMS/src/views/EditGame.jsx
+++ client/CMS/src/views/EditGame.jsx
@@ -1,6 +1,133 @@
-import { Link } from "react-router";
+import { Link, useNavigate, useParams } from "react-router";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import gifLoading from "../assets/Pulse-1s-284px.svg";
+import Toastify from "toastify-js";
+import { fetchCategoriesAsync } from "../features/categories/categories-slice";
 
 export default function EditGame() {
+  const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [developer, setDeveloper] = useState("");
+  const [image, setImage] = useState("");
+  const [CategoryId, setCategoryId] = useState("");
+  const { categories, loading, error } = useSelector((state) => state.categoriesReducer);
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  const fetchGame = async () => {
+    try {
+      const { data } = await axios({
+        method: "GET",
+        url: `http://localhost:3000/games/${id}`,
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+      });
+
+      // console.log(data);
+
+      setTitle(data.game.title);
+      setDeveloper(data.game.developer);
+      setImage(data.game.image);
+      setCategoryId(data.game.CategoryId);
+    } catch (error) {
+      Toastify({
+        text: `${error.response.data.message}`,
+        duration: 3000,
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #ff416c, #ff4b2b)",
+        },
+        onClick: function () {}, // Callback after click
+      }).show;
+    }
+  };
+
+  useEffect(() => {
+    fetchGame();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios({
+        method: "PUT",
+        url: `http://localhost:3000/games/${id}`,
+        data: {
+          title,
+          developer,
+          image,
+          CategoryId,
+        },
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+      });
+
+      // console.log(data);
+
+      Toastify({
+        text: `${data.message}`,
+        duration: 3000,
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function () {}, // Callback after click
+      }).showToast();
+
+      navigate("/");
+    } catch (error) {
+      Toastify({
+        text: `${error.response.data.message}`,
+        duration: 3000,
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function () {}, // Callback after click
+      }).showToast();
+    }
+  };
+
+  useEffect(() => {
+    dispatch(fetchCategoriesAsync());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (error) {
+      Toastify({
+        text: `${error}`,
+        duration: 3000,
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #ff416c, #ff4b2b)",
+        },
+        onClick: function () {}, // Callback after click
+      }).showToast();
+    }
+  }, [error]);
+
+  if (loading) {
+    return (
+      <>
+        <section className="flex justify-center items-center">
+          <img src={gifLoading} />
+        </section>
+      </>
+    );
+  }
+
   return (
     <>
       <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -13,7 +140,7 @@ export default function EditGame() {
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 shadow-lg sm:rounded-lg sm:px-10">
             {/* Add Game Form */}
-            <form className="space-y-6" action="#" method="POST" encType="multipart/form-data">
+            <form onSubmit={handleSubmit} className="space-y-6" action="#" method="POST" encType="multipart/form-data">
               {/* Game Title */}
               <div>
                 <label htmlFor="title" className="block text-sm font-medium text-gray-700">
@@ -24,6 +151,8 @@ export default function EditGame() {
                   <input
                     id="title"
                     name="title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                     type="text"
                     required=""
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
@@ -40,6 +169,8 @@ export default function EditGame() {
                   <input
                     id="developer"
                     name="developer"
+                    value={developer}
+                    onChange={(e) => setDeveloper(e.target.value)}
                     type="text"
                     required=""
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
@@ -57,12 +188,19 @@ export default function EditGame() {
                   <select
                     id="categoryId"
                     name="categoryId"
-                    required=""
+                    value={CategoryId || "DEFAULT"}
+                    required
+                    onChange={(e) => setCategoryId(e.target.value)}
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                   >
-                    <option value="">Select a category</option>
-                    <option value={1}>PC Game</option>
-                    <option value={2}>Mobile Game</option>
+                    <option value="DEFAULT" disabled hidden>
+                      Select a category
+                    </option>
+                    {categories?.map((category) => (
+                      <option key={category.id} value={category.id}>
+                        {category.name}
+                      </option>
+                    ))}
                   </select>
                 </div>
               </div>
@@ -76,6 +214,8 @@ export default function EditGame() {
                   <input
                     id="image"
                     name="image"
+                    value={image}
+                    onChange={(e) => setImage(e.target.value)}
                     type="text"
                     required=""
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
@@ -95,7 +235,7 @@ export default function EditGame() {
                   type="submit"
                   className="w-full flex justify-center py-2 px-4 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                 >
-                  Add Game
+                  Edit Game
                 </button>
               </div>
             </form>
