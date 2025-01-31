9a207c6b5fa8d0e948bd66060000d75df95cd077
robbyxsuganda <robbyxsuganda@gmail.com>
2025-01-31 11:52:40 +0700

    fix: home import on app

--- client/Public/src/App.jsx
+++ client/Public/src/App.jsx
@@ -2,11 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import Login from "./views/Login";
 import Register from "./views/Register";
 import BaseLayout from "./views/BaseLayout";
-import Home from "./views/home";
 import Games from "./views/Games";
 import TopUp from "./views/TopUp";
 import Profile from "./views/Profile";
 import Transaction from "./views/Transaction";
+import Home from "./views/Home";
 
 export default function App() {
   return (
